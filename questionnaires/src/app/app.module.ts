3985d41aee212f73af197d824067c7edaa8129e1
margiebed <margiebed@gmail.com>
2020-04-27 13:53:24 +0200

    Add configuration with firebase

--- questionnaires/src/app/app.module.ts
+++ questionnaires/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { CoreModule } from './core/core.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { environment } from "./../environments/environment";
+import { CoreModule } from "./core/core.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "angularfire2";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
