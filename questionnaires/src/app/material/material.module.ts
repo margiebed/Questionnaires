d0e9a6418fa803742f147ea1e5ecf8b3a1411058
margiebed <margiebed@gmail.com>
2020-04-27 15:08:53 +0200

    Add MaterialModule changes

--- questionnaires/src/app/material/material.module.ts
+++ questionnaires/src/app/material/material.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-import {MatIconModule,
-   MatCardModule,
-   MatFormFieldModule,
-   MatInputModule,
-   MatDialogModule,
-   MatButtonModule,
-   MatSnackBarModule,
-   MatCheckboxModule,
-   MatSelectModule,
-   MatToolbarModule} from '@angular/material';
+import { CommonModule } from '@angular/common';
+import {MatIconModule } from '@angular/material/icon';
+import {MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule} from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 const MATERIAL_MODULES = [
-  MatInputModule,
+  MatIconModule,
   MatCardModule,
   MatFormFieldModule,
   MatInputModule,
@@ -27,7 +28,7 @@ const MATERIAL_MODULES = [
 @NgModule({
   declarations: [],
   imports: [
-
+    CommonModule
   ],
   exports:[...MATERIAL_MODULES],
 })
